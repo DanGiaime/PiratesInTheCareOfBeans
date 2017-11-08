ccb96e733ede618137c0aed0ea4c18b5c333fcb5
Dom <dgl6789@g.rit.edu>
2017-11-08 12:46:56 -0500

    Added pirate, squid, and skeleton animations. Added graphics to source.

---
+++ PitCoB/Assets/ForceAnimationTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceAnimationTest : MonoBehaviour {
+
+    [SerializeField]
+    float forceMax;
+
+    [SerializeField]
+    bool controlForce;
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 force = new Vector3();
+
+        if (controlForce) {
+            force.x += Input.GetAxisRaw("Horizontal");
+            force.y += Input.GetAxisRaw("Vertical");
+        }
+
+        force *= forceMax;
+
+        anim.SetFloat("ForceX", force.x);
+        anim.SetFloat("ForceY", force.y);
+    }
+}
