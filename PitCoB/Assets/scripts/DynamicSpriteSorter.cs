8eb04d9d7403f2c2b48573f80e8f0ca1571987a1
Dominic Liotti (RIT Student) <dgl6789@ad.rit.edu>
2017-11-09 09:55:01 -0500

    Gave Dan his cool scene.

---
+++ PitCoB/Assets/scripts/DynamicSpriteSorter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DynamicSpriteSorter : MonoBehaviour {
+
+    [SerializeField]
+    bool recalcOnUpdate;
+
+    [SerializeField]
+    int orderOverride;
+
+    // Use this for initialization
+    void Start() {
+        Recalculate();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (recalcOnUpdate)
+            Recalculate();
+    }
+
+    void Recalculate() {
+        transform.position = new Vector3(transform.position.x, transform.position.y, -orderOverride + transform.position.y);
+    }
+}
